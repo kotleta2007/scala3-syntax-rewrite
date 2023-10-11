1d06c2f0891160662bc91f67019677ad88ec220e
Mark <kotleta2007@protonmail.ch>
2023-10-11 23:31:52 +0200

    Cleaned up tests

--- output/src/main/scala/fix/Rule1.scala
+++ output/src/main/scala/fix/Rule1.scala
@@ -2,27 +2,24 @@ package fix
 
 object Rule1_Test {
   val x = 4
-
-  val comparison = if x == 0 then "equal" else "not equal"
-  val oneliner = if x == 1 then println(x)
-
-  val loop = while x >= 0 do { println(x) }
   val ys = List(1, 2, 3)
+
+  // rule 1.1
+  val ifThenElse = if x == 0 then "equal" else "not equal"
+  val ifThen = if x == 1 then println(x)
+
+  // rule 1.2
+  val whileLoop = while x >= 0 do { println(x) }
+
+  // rule 1.3
   val forYield = for y <- ys if y > 0 yield x * x
   
+  // rule 1.4
   val forLoop = for y <- ys do { println(y) }
 
+  // rule 1.5
   val tryCatch = 
-    try println(x) catch case ex: Exception => println(x)
+    try println(x) catch case ex: Exception => println(ex.toString())
     
-  /*
-  val comparison = 
-    if (x < 0) {
-      "negative"
-    } else if (x == 0) {
-      "zero"
-    } else {
-      "positive"
-    }
-  */
+  val done = "done" // otherwise, VS Code trims the tabulation on the last line
 }
